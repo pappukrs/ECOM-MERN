855c4965f64f85b522b13cae767faa12bb5e100b
Pappu Kumar <digitalpappu101@gmail.com>
2022-09-29 07:54:22 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+const App = () => {
+  return (
+    <Container>
+      E-COM APP
+    </Container>
+  )
+}
+
+export default App
